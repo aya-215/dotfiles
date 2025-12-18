5319b90fd19cec6604891986abbc84106e029a17
aya-215 <aya.chr928@gmail.com>
2025-12-18 09:33:33 +0900

    feat: conform.nvimを追加して保存時自動フォーマットを実現

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        "<leader>cf",
+        function()
+          require("conform").format({ async = true, lsp_fallback = true })
+        end,
+        mode = { "n", "v" },
+        desc = "Format buffer",
+      },
+    },
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        css = { "prettier" },
+        scss = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        jsonc = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        lua = { "stylua" },
+        python = { "black" },
+        sh = { "shfmt" },
+        bash = { "shfmt" },
+        zsh = { "shfmt" },
+      },
+      format_on_save = {
+        timeout_ms = 1000,
+        lsp_fallback = true,
+      },
+      formatters = {
+        prettier = {
+          prepend_args = { "--single-quote", "--trailing-comma", "es5" },
+        },
+        shfmt = {
+          prepend_args = { "-i", "2" },
+        },
+      },
+    },
+  },
+}
