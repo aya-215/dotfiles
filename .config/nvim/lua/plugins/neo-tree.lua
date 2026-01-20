fec11db1a74999777b9912154b1c875053fac3e8
aya-215 <aya.chr928@gmail.com>
2026-01-20 12:05:45 +0900

    feat: Neo-tree設定を拡充して隠しファイル表示を有効化
    - 隠しファイル(dotfiles)を常に表示
    - Gitステータス表示と色分けを有効化
    - LSP診断情報の表示を有効化
    - キーマッピングを追加 (<leader>E, <leader>ge, <leader>be)
    - ファイル変更の自動検知を有効化
    - インデントマーカーとGitアイコンを設定

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -7,14 +7,78 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
   },
+  keys = {
+    { "<leader>E", "<cmd>Neotree toggle<cr>", desc = "Toggle Neo-tree" },
+    { "<leader>ge", "<cmd>Neotree git_status<cr>", desc = "Git Explorer" },
+    { "<leader>be", "<cmd>Neotree buffers<cr>", desc = "Buffer Explorer" },
+  },
   opts = {
+    close_if_last_window = true,
+    popup_border_style = "rounded",
+    enable_git_status = true,
+    enable_diagnostics = true,
+    sort_case_insensitive = false,
+
+    default_component_configs = {
+      indent = {
+        indent_size = 2,
+        with_markers = true,
+        indent_marker = "│",
+        last_indent_marker = "└",
+        with_expanders = true,
+        expander_collapsed = "",
+        expander_expanded = "",
+      },
+      name = {
+        use_git_status_colors = true,
+      },
+      git_status = {
+        symbols = {
+          added = "",
+          modified = "",
+          deleted = "✖",
+          renamed = "󰁕",
+          untracked = "",
+          ignored = "",
+          unstaged = "󰄱",
+          staged = "",
+          conflict = "",
+        },
+      },
+    },
+
     filesystem = {
       follow_current_file = { enabled = true },
-      hijack_netrw_behavior = "disabled", -- oilと競合しないように
+      hijack_netrw_behavior = "disabled",
+      use_libuv_file_watcher = true,
+      filtered_items = {
+        visible = false,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          ".DS_Store",
+          "thumbs.db",
+          "desktop.ini",
+        },
+        never_show = {
+          ".DS_Store",
+          "thumbs.db",
+          "desktop.ini",
+        },
+      },
     },
+
     window = {
       position = "left",
       width = 35,
+      mappings = {
+        ["<space>"] = { "toggle_node", nowait = false },
+        ["P"] = { "toggle_preview", config = { use_float = true } },
+        ["H"] = "toggle_hidden",
+        ["/"] = "fuzzy_finder",
+        ["[g"] = "prev_git_modified",
+        ["]g"] = "next_git_modified",
+      },
     },
   },
 }
