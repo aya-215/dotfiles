435bec50b54347fe971a93dde7a35034a669a441
aya-215 <aya.chr928@gmail.com>
2026-01-26 11:08:04 +0900

    feat: Git設定をHome Managerで管理
    - programs.gitでグローバル設定を宣言的に管理
    - グローバル: eBASE-Mori、dotfilesローカル: aya-215
    - deltaの設定も移行

--- home.nix
+++ home.nix
@@ -13,7 +13,6 @@
 
   # インストールするパッケージ
   home.packages = with pkgs; [
-    git
     vim
     curl
 
@@ -27,5 +26,43 @@
     lazygit    # Git TUI
     zoxide     # スマートcd
     nb         # ノート管理
+    delta      # Git diff viewer
   ];
+
+  # Git設定
+  programs.git = {
+    enable = true;
+
+    settings = {
+      user = {
+        name = "eBASE-Mori";
+        email = "mori.a@ebase.co.jp";
+      };
+      init.defaultBranch = "main";
+      core = {
+        pager = "delta";
+        excludesfile = "~/.config/git/ignore";
+      };
+      interactive.diffFilter = "delta --color-only";
+      delta = {
+        navigate = true;
+        side-by-side = false;
+        line-numbers = true;
+        syntax-theme = "Monokai Extended";
+        features = "decorations";
+      };
+      "delta \"decorations\"" = {
+        commit-decoration-style = "bold yellow box ul";
+        file-style = "bold yellow ul";
+        file-decoration-style = "none";
+        hunk-header-decoration-style = "cyan box ul";
+      };
+      merge.conflictstyle = "diff3";
+      diff.colorMoved = "default";
+      credential."https://github.com".helper = "!/usr/bin/gh auth git-credential";
+      credential."https://gist.github.com".helper = "!/usr/bin/gh auth git-credential";
+      coderabbit.machineId = "cli/96c13c10b3db406c9ed1d8b2fb783b2c";
+      ghq.root = "~/src";
+    };
+  };
 }
