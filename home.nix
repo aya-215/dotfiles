b6785045fc444ab0546d10803051e2e23d7d9d60
aya-215 <aya.chr928@gmail.com>
2026-01-26 11:08:47 +0900

    feat: 環境変数をHome Managerで管理
    - EDITOR, VISUAL, LANGを設定
    - sessionPath でCargoとローカルbinを追加

--- home.nix
+++ home.nix
@@ -29,6 +29,18 @@
     delta      # Git diff viewer
   ];
 
+  # 環境変数
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+    LANG = "ja_JP.UTF-8";
+  };
+
+  home.sessionPath = [
+    "$HOME/.cargo/bin"
+    "$HOME/.local/bin"
+  ];
+
   # Git設定
   programs.git = {
     enable = true;
